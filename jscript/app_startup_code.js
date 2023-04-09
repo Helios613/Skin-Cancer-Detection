15f45f464a4b5949ccdd6c747b7c7f51ad64e647
Piyush Rajput <82579541+Helios613@users.noreply.github.com>
2023-04-09 23:22:55 +0530

    Update app_startup_code.js

--- jscript/app_startup_code.js
+++ jscript/app_startup_code.js
@@ -39,8 +39,8 @@ let model;
 	// Hide the model loading spinner
 	// This line of html gets hidden:
 	// <div class="progress-bar">Ai is Loading...</div>
-	$('.progress-bar').hide();
-
+	//$('.progress-bar').hide();
+	$('.progress-bar').append(`<p>${model.containerNodes.size}</p>`);
 
 	// Simulate a click on the predict button.
 	// Make a prediction on the default front page image.

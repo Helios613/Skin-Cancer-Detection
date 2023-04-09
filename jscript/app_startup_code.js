4f8dc3c169cd89dc1e45f3c2fe4a20f0119a38ae
Piyush Rajput <82579541+Helios613@users.noreply.github.com>
2023-04-09 20:36:34 +0530

    Update app_startup_code.js

--- jscript/app_startup_code.js
+++ jscript/app_startup_code.js
@@ -34,7 +34,7 @@ let model;
 (async function () {
 
 	model = await tf.loadModel("../final_model_kaggle_version1/model.json");
-	$("#selected-image").attr("src", "http://skin.test.woza.work/assets/samplepic.jpg");
+	$("#selected-image").attr("src", "../assets/samplepic.jpg");
 
 	// Hide the model loading spinner
 	// This line of html gets hidden:
